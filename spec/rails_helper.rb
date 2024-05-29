0bba8916df121544822c4849228b7df442e0d51e
1Popla <marcin.poplawski988@gmail.com>
2024-05-29 15:25:22 +0200

    Add bookings and post tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-# spec/rails_helper.rb
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
